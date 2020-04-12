046ce1c231eb97b955cb165868292a7fce3ec464
patrick.pierre000 <patrick.pierre000@gmail.com>
2020-04-12 00:37:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board()
+=begin def display_board()
   pipe = "   |   |  "
   space = "\n"
   border_dashes = "-"*11
@@ -14,3 +14,10 @@ def display_board()
 end
 
 puts display_board
+=end
+
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
